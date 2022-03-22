882171cad42ce27a4ef754f75e48186be3acf51c
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2022-03-22 21:57:33 +0700

    add ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-require('./bootstrap');
+// require('./bootstrap');
 $(window).on("load",function(){
     $(".loader-wrapper").delay(2500).fadeOut("slow");
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script language="javascript" src="../js/app.js"></script>
-	<link rel="stylesheet" type="text/css" href="../css/app.css">
+    <script language="javascript" src="js/app.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/app.css">
 </head>
 <body>
 	<div class="loader-wrapper">
