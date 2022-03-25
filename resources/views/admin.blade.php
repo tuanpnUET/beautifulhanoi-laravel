3264f3f6c32dc015324d06263358548aa923e303
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2022-03-25 09:22:01 +0700

    feat: add category CRUD

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,4 +1,4 @@
 <h1 style="display: flex; justify-content: center;  margin-top: 10px" >Welcome admin!</h1>
 <a href='<?php echo route('category.index') ?>' class='btn btn-primary'>Category Dashboard</a><br/>
 
-<a href='<?php echo route('destination.index') ?>' class='btn btn-primary'>Destination Dashboard</a><br/>
+<a href='#' class='btn btn-primary'>Destination Dashboard</a><br/>
--- routes/web.php
+++ routes/web.php
@@ -40,92 +40,92 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
-// Route::get('/unicode', function() {
-//     $user = new User();
-//     $allUser = $user::all();
-//     dd($allUser);
-// });
+Route::get('/unicode', function() {
+    $user = new User();
+    $allUser = $user::all();
+    dd($allUser);
+});
 
-// Route::get('/home-1', 'App\Http\Controllers\HomeController@index')->name('home');
-// Route::get('/home-2', [HomeController::class, 'index'])->name('home');
-// Route::get('/show-form', function() {
-//     return view('forms.form');
-// });
+Route::get('/home-1', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/home-2', [HomeController::class, 'index'])->name('home');
+Route::get('/show-form', function() {
+    return view('forms.form');
+});
 
-// Route::get('/test', function() {
-//     return view('forms.form');
-// });
-// Route::post('/test', function() {
-//     return 'call action post of path /test';
-// });
-// Route::put('/test', function() {
-//     return 'call action put of path /test';
-// });
-// Route::delete('/test', function() {
-//     return 'call action delete of path /test';
-// });
-// Route::patch('/test', function() {
-//     return 'call action patch of path /test';
-// });
-// Route::match(['post', 'put'], 'test', function() {
-//     return $_SERVER['REQUEST_METHOD'];
-// });
-// Route::any('test', function() {
-//     $request = new Request;
-//     return $request->method();
-//     // return $_SERVER['REQUEST_METHOD'];
-// });
-// Route::any('test', function(Request $request) {
-//     return $request->method();
-//     // return $_SERVER['REQUEST_METHOD'];
-// });
-// Route::redirect('test', 'show-form');
-// Route::view('show-form', 'form');
-// Route::prefix('admin')->group(function(){
+Route::get('/test', function() {
+    return view('forms.form');
+});
+Route::post('/test', function() {
+    return 'call action post of path /test';
+});
+Route::put('/test', function() {
+    return 'call action put of path /test';
+});
+Route::delete('/test', function() {
+    return 'call action delete of path /test';
+});
+Route::patch('/test', function() {
+    return 'call action patch of path /test';
+});
+Route::match(['post', 'put'], 'test', function() {
+    return $_SERVER['REQUEST_METHOD'];
+});
+Route::any('test', function() {
+    $request = new Request;
+    return $request->method();
+    // return $_SERVER['REQUEST_METHOD'];
+});
+Route::any('test', function(Request $request) {
+    return $request->method();
+    // return $_SERVER['REQUEST_METHOD'];
+});
+Route::redirect('test', 'show-form');
+Route::view('show-form', 'form');
+Route::prefix('admin')->group(function(){
 
-//     Route::get('/', function() {
-//         return view('admin');
-//     })->name('admin');
+    Route::get('/', function() {
+        return view('admin');
+    })->name('admin');
 
-//     Route::get('/test/{id?}', function($id=null) {
-//         $content = 'Call action get of path /test';
-//         $content.=' id = '.$id;
-//         return $content;
-//     });
+    Route::get('/test/{id?}', function($id=null) {
+        $content = 'Call action get of path /test';
+        $content.=' id = '.$id;
+        return $content;
+    });
 
-//     Route::get('/test/{slug}-{id}.html', function($slug=null, $id=null) {
-//         $content = 'Call action get of path /test';
-//         $content.=' id = '.$id.'<br/>';
-//         $content.=' slug = '.$slug;
-//         return $content;
-//     })->where(
-//         [
-//             'slug' => '.+',
-//             'id' => '[0-9]+',
-//         ]
-//     );
-//     Route::get('/detail/{slug}-{id?}.html', function($slug=null, $id=null) {
-//         $content = 'Call action get of path /detail';
-//         $content.=' id = '.$id.'<br/>';
-//         $content.=' slug = '.$slug;
-//         return $content;
-//     })->where('slug', '.+')->where('id', '[0-9]+')->name('admin.detail.id');
+    Route::get('/test/{slug}-{id}.html', function($slug=null, $id=null) {
+        $content = 'Call action get of path /test';
+        $content.=' id = '.$id.'<br/>';
+        $content.=' slug = '.$slug;
+        return $content;
+    })->where(
+        [
+            'slug' => '.+',
+            'id' => '[0-9]+',
+        ]
+    );
+    Route::get('/detail/{slug}-{id?}.html', function($slug=null, $id=null) {
+        $content = 'Call action get of path /detail';
+        $content.=' id = '.$id.'<br/>';
+        $content.=' slug = '.$slug;
+        return $content;
+    })->where('slug', '.+')->where('id', '[0-9]+')->name('admin.detail.id');
 
-//     Route::get('/show-form', function() {
-//         return view('forms.form');
-//     })->name('show-form');
+    Route::get('/show-form', function() {
+        return view('forms.form');
+    })->name('show-form');
 
-//     Route::prefix('/product')->middleware('checkpermission')->group(function(){
-//         Route::get('/', function(){
-//             return 'list product';
-//         })->name('admin.product');
+    Route::prefix('/product')->middleware('checkpermission')->group(function(){
+        Route::get('/', function(){
+            return 'list product';
+        })->name('admin.product');
         
-//         Route::get('/add', function(){
-//             return 'add product';
-//         })->name('admin.product.add');
+        Route::get('/add', function(){
+            return 'add product';
+        })->name('admin.product.add');
 
-//     });
-// });
+    });
+});
 
 // article route
 Route::get('/article-list', [ArticleController::class, 'index'])->name('article.index');
@@ -136,6 +136,7 @@ Route::post('/edit-article', [ArticleController::class, 'update'])->name('articl
 Route::get('/delete-article/{id}', [ArticleController::class, 'show'])->name('article.show');
 Route::post('/delete-article', [ArticleController::class, 'delete'])->name('article.delete');
 
+// client router
 Route::get('/', function () {
     return view('index');
 })->name('home');
@@ -160,13 +161,13 @@ Route::get('/travel', function () {
     return view('travel');
 })->name('travel');
 
-
+// admin router
 Route::prefix('admin')->group(function() {
     Route::get('/', function () {
         return view('admin');
     });
     
-    Route::get('/create-list', [CategoryController::class, 'index'])->name('category.index');
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
     Route::get('/create-category', [CategoryController::class, 'create'])->name('category.create');
     Route::post('/create-category', [CategoryController::class, 'store'])->name('category.store');
     Route::resource('category', CategoryController::class);
